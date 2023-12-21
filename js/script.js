ef94c8d517e27ef3d1091aedcd2d7542bc71c649
Ben-Hur <138973267+botlorien@users.noreply.github.com>
2023-12-21 19:28:30 -0400

    21-12-2023

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Hardcoded list of contacts
 const contacts = [
-    { name: "Rev. Rubens", campo: "Argentina", contato: "+55 65 9906-5900", Pix: "57220840187", img:"../assets/rubens.jpeg" },
-    { name: "Rev. Terena", campo: "Campo Grande", contato: "+55 67 8190-2539", Pix: "hterena@gmail.com" , img:"../assets/terena.jpeg"},
-    { name: "Rev. Jorge", campo: "Libano", contato: "+961 81 797 073", Pix: "64999102304" , img:"../assets/jorge.jpeg"},
-    { name: "Rev. Marcos", campo: "Nova Zelândia", contato: "+64 22 103 8126", Pix: "621309781-34" , img:"../assets/marcos.jpeg"},
-    { name: "Rev. Basilio", campo: "Quilombolas SP", contato: "+221 77 187 21 43", Pix: "56971192168" , img:"../assets/basilio.jpeg"},
-    { name: "Rev. Bira kulina", campo: "Acre", contato: "+55 68 9959-2530", Pix: "068999592530" , img:"../assets/bira.jpeg"},
-    { name: "Rev. Luiz Henrique", campo: "Ruanda", contato: "+55 13 99775-0512", Pix: "50.917.306/0001-85", img:"../assets/luiz.jpeg" }
+    { name: "Rev. Rubens", campo: "Argentina", contato: "+55 65 9906-5900", Pix: "57220840187", img:"assets/rubens.jpeg" },
+    { name: "Rev. Terena", campo: "Campo Grande", contato: "+55 67 8190-2539", Pix: "hterena@gmail.com" , img:"assets/terena.jpeg"},
+    { name: "Rev. Jorge", campo: "Libano", contato: "+961 81 797 073", Pix: "64999102304" , img:"assets/jorge.jpeg"},
+    { name: "Rev. Marcos", campo: "Nova Zelândia", contato: "+64 22 103 8126", Pix: "621309781-34" , img:"assets/marcos.jpeg"},
+    { name: "Rev. Basilio", campo: "Quilombolas SP", contato: "+221 77 187 21 43", Pix: "56971192168" , img:"assets/basilio.jpeg"},
+    { name: "Rev. Bira kulina", campo: "Acre", contato: "+55 68 9959-2530", Pix: "068999592530" , img:"assets/bira.jpeg"},
+    { name: "Rev. Luiz Henrique", campo: "Ruanda", contato: "+55 13 99775-0512", Pix: "50.917.306/0001-85", img:"assets/luiz.jpeg" }
 ];
 
 function displayRandomContact() {
